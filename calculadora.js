24d622f5f221a663219e5600de77253e9e28e32d
Nataly Carvalho <natalycarvalho404@gmail.com>
2023-03-28 20:29:05 -0300

    Delete calculadora.js

--- calculadora.js
+++
@@ -1,9 +0,0 @@
-function calcularSalario(){
-    const Htrababalhadas = document.getElementById("horas-trabalhadas").value;
-    const salarioH = document.getElementById("salario-hora").value;
-    const diasT = document.getElementById("DiasTrabalhados").value;
-    const salarioTotal = Htrababalhadas * salarioH *diasT;
-
-    const resultado = document.getElementById("Resultado");
-    resultado.innerHTML = `Salario total: R$ ${salarioTotal.toFixed(2)}`;
-}

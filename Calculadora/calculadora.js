860baeab30421c64c9022f7fb9b4f2d6e677e190
Nataly Carvalho <natalycarvalho404@gmail.com>
2023-03-28 20:29:54 -0300

    Add files via upload

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,7 +1,8 @@
 function calcularSalario(){
-    const Htrababalhadas = document.getElementById("horas-trabalhadas").value;
-    const salarioH = document.getElementById("salario-hora").value;
-    const salarioTotal = Htrababalhadas * salarioH;
+    const ganhoMes = document.getElementById("horas-trabalhadas").value;
+    const horasDias = document.getElementById("salario-hora").value;
+    const diasMes = document.getElementById("DiasTrabalhados").value;
+    const salarioTotal = ganhoMes /(horasDias *diasMes);
 
     const resultado = document.getElementById("Resultado");
     resultado.innerHTML = `Salario total: R$ ${salarioTotal.toFixed(2)}`;
